d47b4c1d31832195f33e0806edecdbe26b4655b5
Azke <e.desbois@hotmail.fr>
2018-09-10 11:40:16 +0200

    review Magus 2

--- parse.js
+++ parse.js
@@ -7,28 +7,26 @@ var exports = module.exports = {};
 
 const games = ['code-of-kutulu', 'code-royale', 'tic-tac-toe', 'botters-of-the-galaxy', 'code4life', 'mean-max', 'wondev-woman', 'coders-of-the-caribbean',  'ghost-in-the-cell', 'fantastic-bits', 'hypersonic', 'codebusters', 'smash-the-code', 'coders-strike-back', 'back-to-the-code', 'great-escape', 'platinum-rift2', 'platinum-rift', 'poker-chip-race', 'game-of-drone', 'tron-battle']
 
-
 exports.compare = async function (players)
 {
+    let results = {};
+    results.players = {};
+    results.games = {};
+    players = players.split(' ');
 
-    let results = {}
-    results.players = {}
-    results.games = {}
-    players = players.split(' ')
-    
     let games_ranks = await Promise.all(games.map(game => getRanksInMulti(game, players)));
     // console.log(player_ranks)        
-    
+
     // stores players
-    results.players = players
+    results.players = players;
     // stores game results
     for (let json of games_ranks)
         for (let game in json)
-            results.games[game] = json[game]
+            results.games[game] = json[game];
 
-    console.log(results)
+    console.log(results);
     // compute firsts
-    return results        
+    return results; 
 }
     
 
@@ -36,20 +34,20 @@ exports.compare = async function (players)
 const league_value_array = ['Legend', 'Gold', 'Silver', 'Bronze', 'Wood', 'no_league']
 function league_value(league)
 {
-    let idx = league_value_array.indexOf(league)
+    let idx = league_value_array.indexOf(league);
     if (idx == -1)
     {
         // impossible case
-        return 0
+        return 0;
     }
-	return idx
+	return idx;
 }
 
 function rank_value(player_rank, game)
 {
     if (typeof player_rank[game] === 'undefined')
-        return 1e10
-    return player_rank[game]['rank'] + 1e4*league_value(player_rank[game]['league'])
+        return 1e10;
+    return player_rank[game]['rank'] + 1e4*league_value(player_rank[game]['league']);
 }
 
 
@@ -58,22 +56,26 @@ const get_league_array = {
     2: 'Gold',
     3: 'Silver',
     4: 'Bronze'
-}
+};
 function get_league(division_index, division_count)
 {
-    return get_league_array[division_count-division_index] || 'Wood' // if less than 4, it is a wood league
+    return get_league_array[division_count-division_index] || 'Wood'; // if less than 4, it is a wood league
 }
 
 function get_cardinal(value)
 {
-    let digit = value.toString().slice(-1)
-    if (digit == '1')
-       return 'st';
-	if (digit == '2')
-        return 'nd';
-	if (digit == '3')
-        return 'rd';
-	return 'th';
+    let digit = value.toString().slice(-1);
+    switch(digit)
+    {
+        case '1':
+            return 'st';
+        case '2':
+            return 'nd';
+        case '3':
+            return 'rd';
+        default:
+            return 'th';
+    }
 }
 
 // "code-royale" -> "Code Royale"
@@ -89,7 +91,7 @@ async function getRanksInMulti (multi, pseudos)
 {
     let path_file = __dirname + '/leaderboards/' + multi + '.json';
 
-    multi = prettify(multi)
+    multi = prettify(multi);
     let readFile = util.promisify(fs.readFile);
     let content = await readFile(path_file);
 
@@ -97,24 +99,24 @@ async function getRanksInMulti (multi, pseudos)
 
     let users = data['success']['users'];
 
-    let pseudoRemaining = pseudos.length
-    let result = {}
-    result[multi] = {}
-    let minValue = 1e21
-    let bestPlayer = ''
-    for (var u of users)
+    let pseudoRemaining = pseudos.length;
+    let result = {};
+    result[multi] = {};
+    let minValue = 1e21;
+    let bestPlayer = '';
+    for (let u of users)
     {
         if (pseudoRemaining == 0)
         {
             if (bestPlayer !== '')
-            result[multi][bestPlayer]['first'] = 'first'
+                result[multi][bestPlayer]['first'] = 'first';
             return result;
         }
 
         // console.log(u)
         if (u['pseudo'])
         {
-            for (var pseudo of pseudos)
+            for (let pseudo of pseudos)
             {
 
                 if (u['pseudo'] === pseudo)
@@ -123,28 +125,28 @@ async function getRanksInMulti (multi, pseudos)
                     if (u['league'])
                         league = get_league(u['league']['divisionIndex'], u['league']['divisionCount']);
                     if (league === '')
-                        league = 'no_league'
+                        league = 'no_league';
                     let rank = u['localRank'];
-                    result[multi][pseudo] = {'rank': rank + get_cardinal(rank), 'league': league, 'first':''}
+                    result[multi][pseudo] = {'rank': rank + get_cardinal(rank), 'league': league, 'first':''};
                     if (rank_value(result[multi][pseudo], multi) < minValue)
                     {
-                        minValue = rank_value(result[multi][pseudo], multi)
-                        bestPlayer = pseudo
+                        minValue = rank_value(result[multi][pseudo], multi);
+                        bestPlayer = pseudo;
                     }
                     pseudoRemaining--;
-                    break
+                    break;
                 }
             }
         }
     }
-    for (pseudo of pseudos)
+    for (let pseudo of pseudos)
     {
         if (typeof result[multi][pseudo] === 'undefined')
         {
-            result[multi][pseudo] = {'rank': undefined, 'league': undefined, 'first':''}
+            result[multi][pseudo] = {'rank': undefined, 'league': undefined, 'first':''};
         }
     }
     if (bestPlayer !== '')
-        result[multi][bestPlayer]['first'] = 'first'
-    return result// player not found
+        result[multi][bestPlayer]['first'] = 'first';
+    return result; // player not found
 }
