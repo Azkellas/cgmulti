a87e737dd4a8cb30d265e321365cd0fbdaa541c5
Azke <e.desbois@hotmail.fr>
2018-09-11 23:09:57 +0200

    vue 0.1

--- node.js
+++ node.js
@@ -1,6 +1,6 @@
 "use strict";
 
-
+var url = require('url')
 var express = require('express');
 var bodyParser = require('body-parser'); // Charge le middleware de gestion des paramètres
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
@@ -13,23 +13,33 @@ var app = express();
 app.use(express.static('./public'))
 
 
+// get with players  (probably ugly as fuck)
+.get('/', urlencodedParser, function(req, res) {
+    res.sendFile('index.html', {root: __dirname });
+})
 
 // request with players
-.post('/players/', urlencodedParser, function(req, res) {
-    parser.compare(req.body.players).then(function(players_data) {
-        res.render('cgmulti.ejs', {data: players_data, _: _});
-    });
-})
+// .post('/players/', urlencodedParser, function(req, res) {
+//     parser.compare(req.body.players).then(function(players_data) {
+//         res.render('cgmulti.ejs', {data: players_data, _: _});
+//     });
+// })
 
 // get with players  (probably ugly as fuck)
 .get('/players/', urlencodedParser, function(req, res) {
-    res.render('cgmulti.ejs', {data: {}, _: _});
+    console.log('get')
+    var queryData = url.parse(req.url, true).query;
+    console.log(queryData);
+    parser.compare(queryData.playersQuery).then(function(players_data) {
+        res.send(players_data);
+    });
+
 })
 
-// redirection
-.use(function(req, res, next){
-    res.redirect('/players/');
-})
+// // redirection
+// .use(function(req, res, next){
+//     res.redirect('/players/');
+// })
 
 
 .listen(8080);
---
+++ public/js/vue.js
@@ -0,0 +1,48 @@
+new Vue({
+	// -------------
+	// APP CONTAINER
+  // -------------
+  el: '#app',
+  
+  // --------
+  // RAW DATA
+  // --------
+  data: {
+    result: {},
+    playersQuery: '',
+  },
+  
+    // ------------
+    // DERIVED DATA
+    // ------------
+  computed: {
+  },
+  
+  // ---------------
+  // LIFECYCLE HOOKS
+  // ---------------
+  created: function () {
+  },
+
+  // --------------
+  // SCOPED METHODS
+  // --------------
+  methods: {
+    getData: function() {
+        var vm = this;
+        event.preventDefault();
+        // alert(this.playersQuery);
+        axios
+        .get('/players/', {
+            params: {
+                playersQuery: this.playersQuery
+            }
+        })
+        .then(function(response) {
+            //console.log(response);
+            vm.result = response.data;
+            //console.log(vm.result)
+        });
+    }
+  }
+})
