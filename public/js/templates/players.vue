7265da8319c4dbd207b78f5ddb39727f4657aee9
Azke <e.desbois@hotmail.fr>
2018-09-16 21:47:03 +0200

    components ok

---
+++ public/js/templates/players.vue
@@ -0,0 +1,167 @@
+<template>
+    <div class="playerRoot">
+        <div class="title">
+            <h1>CG multi</h1>
+        </div>
+        <form action="/players/" method="post" v-on:submit.prevent="getData">
+            <input type="text" v-model="playersQuery" placeholder="player1 player2 player3" id="playersQuery" class="form-control">
+            <input type="submit" style="visibility: hidden;" />
+        </form>
+        <!-- if a request has been made-->
+        <span v-if="!_.isEmpty(result)">
+            <!-- if some players were not found -->
+            <span v-if="result.players_not_found.length">
+                <span v-for="player of result.players_not_found" :key="player">
+                    Player "{{player}}" could not be found <br />
+                </span>
+            </span>
+
+            <!--  if the table is not empty-->
+            <table v-if="result.players_found.length" class="table table-dark table-striped table-bordered table-hover table-condensed table-sm" style="width:auto;" align="center">
+                <thead>
+                    <tr>
+                        <th scope="col"></th>
+                        <th v-for="player of result.players_found" :key="player">{{player}}</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="(players, game) in result['games']" :key="game">
+                        <!-- game -->
+                        <th scope="row"><span class="game"><a :href='getLink(game)' target="_blank">{{prettify(game)}}</a></span></th>
+                        <!-- displays players ranks-->
+                        <td v-for="player of result.players_found" :class="tdClass(players[player])" :key="player">
+                            <!-- player is rank in the given multi-->
+                            <span v-if="players[player]">
+                                <span :class="players[player]['league']" v-tooltip="printDate(players[player]['date'])">
+                                    <!-- rank -->
+                                    {{players[player]["rank"]}} 
+                                    <!-- league-->
+                                    <span v-if="players[player].league !== 'no_league'"> {{players[player]["league"]}}</span>
+                                </span>
+                            </span>
+                            <!-- player is not rank in the given multi-->
+                            <span v-else class="undefined"></span>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </span>
+    </div>
+</template>
+
+<style>
+</style>
+
+<script>
+const baseUrl = 'https://www.codingame.com/multiplayer/bot-programming/';
+
+module.exports = {
+  data: function() {
+    return {
+      // result: {'players_found':['Azke'],'players_not_found':[], 'games':{'coc':{'Azke':{'rank':'1','first':'','league':'Legend'}}}},
+      result: {},
+      playersQuery: "",
+      route: window.location,
+      locale: window.navigator.userLanguage || window.navigator.language
+    };
+  },
+
+  // ------------
+  // DERIVED DATA
+  // ------------
+  computed: {},
+
+  // ---------------
+  // LIFECYCLE HOOKS
+  // ---------------
+  created: function() {
+    // this.$router.push('/players')
+    this.initialData(this.route);
+
+    // get local date format while writing in english
+    moment.locale(this.locale);
+    var data = moment.localeData()._longDateFormat;
+    moment.locale("en-US");
+    moment.localeData()._longDateFormat = data;
+  },
+
+  watch: {
+    // whenever question changes, this function will run
+    route: function(newRoute, oldRoute) {
+      console.log("new route");
+      this.initialData(newRoute);
+    }
+  },
+
+  // --------------
+  // SCOPED METHODS
+  // --------------
+  methods: {
+    getData: function() {
+      var vm = this;
+      window.history.pushState(
+        "players",
+        "CG multi",
+        "/players" + "?" + this.playersQuery
+      );
+      //this.$router.push('/players')
+
+      axios
+        .get("/playersQuery/", {
+          params: {
+            playersQuery: this.playersQuery.replace(/[\ ]+/g, " ")
+          }
+        })
+        .then(function(response) {
+          vm.result = response.data;
+        });
+    },
+
+    initialData: async function(route) {
+      var vm = this;
+      console.log("initdata ! " + route);
+      if (route.pathname !== "/players") {
+        vm.result = {};
+        return;
+      }
+      players = route.search.substr(1);
+      players = players.replace(/%20/g, " ");
+      vm.playersQuery = players;
+      axios
+        .get("/playersQuery/", {
+          params: {
+            playersQuery: players
+          }
+        })
+        .then(function(response) {
+          vm.result = response.data;
+        });
+    },
+
+    printDate: function(date) {
+      date = moment(date);
+      return date.calendar();
+    },
+
+    tdClass: function(player) {
+      if (this.result.players_found.length < 2) return "";
+      if (player === undefined) return "";
+      if (player.first !== "") return "first";
+      return "";
+    },
+
+    // "code-royale" -> "Code Royale"
+    prettify: function(game) {
+      return game
+        .split("-")
+        .map(word => word[0].toUpperCase() + word.substr(1))
+        .join(" ");
+    },
+
+    getLink: function(game) {
+      return baseUrl + game + "/";
+    }
+  }
+};
+</script>
+
--- public/js/vue.js
+++ public/js/vue.js
@@ -1,109 +1,13 @@
-const baseUrl = 'https://www.codingame.com/multiplayer/bot-programming/'
-
-
-
-new Vue({
-	// -------------
+const app = new Vue({
+    el: '#app',
+    // -------------
 	// APP CONTAINER
   // -------------
-  el: '#app',
+  // router,
   // --------
+  components: {
+      'players': httpVueLoader('js/templates/players.vue')
+  },
   // RAW DATA
   // --------
-  data: {
-    result: {},
-    playersQuery: '',
-    route: window.location,
-    locale: window.navigator.userLanguage || window.navigator.language
-  },
-  
-    // ------------
-    // DERIVED DATA
-    // ------------
-  computed: {
-  },
-  
-  // ---------------
-  // LIFECYCLE HOOKS
-  // ---------------
-  created: function () {
-    this.initialData(this.route);
-
-    // get local date format while writing in english
-    moment.locale(this.locale);
-    var data = moment.localeData()._longDateFormat;
-    moment.locale('en-US');
-    moment.localeData()._longDateFormat = data;
-  },
-
-  // --------------
-  // SCOPED METHODS
-  // --------------
-  methods: {
-    getData: function() {
-        var vm = this;
-        window.history.replaceState('players', 'CG multi', '/players'+'?'+this.playersQuery)
-        axios
-        .get('/playersQuery/', {
-            params: {
-                playersQuery: this.playersQuery.replace(/[\ ]+/g," ")
-            }
-        })
-        .then(function(response) {
-            vm.result = response.data;
-        });
-    },
-
-    initialData: async function(route) {
-      var vm = this;
-      if (route.pathname !== '/players')
-      {
-        vm.result = {};
-        return;
-      }
-      players = route.search.substr(1);
-      players = players.replace(/%20/g," ");
-      vm.playersQuery = players;
-      axios
-      .get('/playersQuery/', {
-          params: {
-              playersQuery: players
-          }
-      })
-      .then(function(response) {
-          vm.result = response.data;
-      });
-    },
-
-    printDate: function(date)
-    {
-        date = moment(date);
-        return date.calendar();
-        return date.format('L');
-    },
-
-    tdClass: function(player)
-    {
-        if (this.result.players_found.length < 2)
-            return '';
-        if (player === undefined)
-            return '';
-        if (player.first !== '')
-            return 'first';
-        else
-            return '';
-    },
-
-    // "code-royale" -> "Code Royale"
-    prettify: function (game)
-    {
-        return game.split('-').map(word => word[0].toUpperCase() + word.substr(1)).join(' ');
-    },
-
-    getLink: function (game)
-    {
-        return baseUrl + game + '/'
-    }
-
-  }
 })
