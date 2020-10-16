7a6e1a3aad9cd9c20b7f02ad47353e0b04b7d853
Crypticsy <animesh19basnet@gmail.com>
2020-10-16 15:24:21 +0545

    Addition of games in board (updt-stat.js & updt-stat-weekly.js) as well as os friendly python code

--- update_leaderboards.py
+++ update_leaderboards.py
@@ -21,7 +21,10 @@ def download_leaderboard(multi):
 		return
 
 	try:
-		with open(dir_path + "/leaderboards/"+multi+".json", "w") as f:
+		if not os.path.exists(os.path.join(dir_path,"leaderboards")):		# create the directory if it doesn't exists 
+			os.mkdir(os.path.join(dir_path,"leaderboards"))
+		
+		with open(os.path.join(dir_path,"leaderboards",multi+".json"), "w") as f:
 			f.write(json.dumps(d))
 	except ValueError:
 		print(str(datetime.now()) + ": could not dump json of " + multi + ": " + ValueError)
@@ -31,7 +34,7 @@ def download_leaderboard(multi):
 if __name__ == "__main__":
 	games = ["a-code-of-ice-and-fire", "code-a-la-mode", "cultist-wars", "bandas", "bit-runner-2048", "yavalath", "langton-s-ant", "checkers", "vindinium", "legends-of-code-magic", "code-of-kutulu", "code-royale", "tic-tac-toe", "botters-of-the-galaxy", "code4life", "mean-max", "wondev-woman", "coders-of-the-caribbean",  "ghost-in-the-cell", "fantastic-bits", "hypersonic", "codebusters", "smash-the-code", "coders-strike-back", "back-to-the-code", "great-escape", "platinum-rift2", "platinum-rift", "poker-chip-race", "game-of-drone", "tron-battle", "xmas-rush"]
 	for game in games:
-		path_file = 'leaderboards/' + game + '.json'
+		path_file = os.path.join("leaderboards", game + '.json')
 
 		# check if the file is recent enough
 		# timestamp = os.stat(path_file).st_mtime
