03522e930b3ebd0743996a52e4382cf4fe2f9929
Azkellas <e.desbois@hotmail.Fr>
2023-02-10 16:17:28 +0100

    Fix update_leaderboards with new api

--- update_leaderboards.py
+++ update_leaderboards.py
@@ -1,53 +1,61 @@
 #!/usr/bin/python3
-import os
 import json
-import sys
-import time
 from datetime import datetime
+from pathlib import Path
+
 import requests
 
-
-dir_path = os.path.dirname(os.path.realpath(__file__))
+dir_path = Path(__file__).parent.resolve()
 
 # updates leaderboards file
-def download_leaderboard(multi):
-	req_t = 'https://www.codingame.com/services/LeaderboardsRemoteService/getFilteredPuzzleLeaderboard'
+def download_leaderboard(multi: str) -> None:
+	req_t = "https://www.codingame.com/services/LeaderboardsRemoteService/getFilteredPuzzleLeaderboard"
 	arg = "[" + multi + ", undefined, global, { active: false, column: undefined, filter: undefined}]"
 
-	r = requests.post(req_t, data=arg)
+	headers={
+		"Content-type":"application/json",
+		"Accept":"application/json",
+	}
+
+	r = requests.post(req_t, data=arg, headers=headers, timeout=2)
 	d = r.json()
 	if "error" in d:
 		print(str(datetime.now()) + ": skip multi " + multi + ", got the API error :" + d["error"]["message"])
 		return
 
 	try:
-		with open(os.path.join(dir_path,"leaderboards",multi+".json"), "w") as f:
+		filepath = Path(dir_path) / "leaderboards" / f"{multi}.json"
+		with filepath.open("w") as f:
 			f.write(json.dumps(d))
 	except ValueError:
 		print(str(datetime.now()) + ": could not dump json of " + multi + ": " + ValueError)
 
-def get_multi_list():
+def get_multi_list() -> list[str]:
 	# Method courtesy of Neumann
 	# First get the global puzzle list
-	req_t = 'https://www.codingame.com/services/PuzzleRemoteService/findAllMinimalProgress'
-	r = requests.post(req_t, data="[null]")
+	headers={
+		"Content-type":"application/json",
+		"Accept":"application/json",
+	}
+	req_t = "https://www.codingame.com/services/Puzzle/findAllMinimalProgress"
+	r = requests.post(req_t, data="[null]", headers=headers, timeout=2)
 	d = r.json()
 	if "error" in d:
-		print(str(datetime.now()) + ": failed to fetch multi list " + multi + ", got the API error :" + d["error"]["message"])
-		return
+		print(str(datetime.now()) + ": failed to fetch multi list, got the API error :" + d["error"]["message"])
+		return None
 
-	multiIds = []
+	multi_ids = []
 	for game in d:
 		if game["level"] == "multi":
-			multiIds.append(game["id"])
+			multi_ids.append(game["id"])
 
 	# Then get the details on every multi
-	req_t = 'https://www.codingame.com/services/PuzzleRemoteService/findProgressByIds'
-	r = requests.post(req_t, data=f"[{multiIds}, null, 1]")
+	req_t = "https://www.codingame.com/services/Puzzle/findProgressByIds"
+	r = requests.post(req_t, data=f"[{multi_ids}, null, 1]", headers=headers, timeout=2)
 	d = r.json()
 	if "error" in d:
-		print(str(datetime.now()) + ": failed to fetch multi list " + multi + ", got the API error :" + d["error"]["message"])
-		return
+		print(str(datetime.now()) + ": failed to fetch multi list, got the API error :" + d["error"]["message"])
+		return None
 
 	multis = []
 	for game in d:
@@ -56,11 +64,10 @@ def get_multi_list():
 	return multis
 
 if __name__ == "__main__":
-
 	games = get_multi_list()
-
-	if not os.path.exists(os.path.join(dir_path,"leaderboards")):		# create the directory if it doesn't exist 
-		os.mkdir(os.path.join(dir_path,"leaderboards"))
+	leaderboards_folder = Path(dir_path) / "leaderboards"
+	if not leaderboards_folder.exists():
+		leaderboards_folder.mkdir()
 
 	for game in games:
 		download_leaderboard(game)
